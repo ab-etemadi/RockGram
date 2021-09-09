f15e8ac703bbfdf32b3c9cafb695c53b0f0052db
Abdulelah Etemadi <80175309+ab-etemadi@users.noreply.github.com>
2021-09-09 18:31:57 +0430

    message controller & service debug done

--- src/rockgram/message/message.controller.ts
+++ src/rockgram/message/message.controller.ts
@@ -1,72 +1,40 @@
-/* eslint-disable prettier/prettier */
-<<<<<<< HEAD:src/rockgram/message/message.controller.ts
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
-=======
+
 import { BadRequestException, Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
-import { ChatService } from './chat.service';
->>>>>>> origin/master:src/chat/chat.controller.ts
+
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
 import { MessageService } from './message.service';
 
 @Controller('message')
-<<<<<<< HEAD:src/rockgram/message/message.controller.ts
+
 export class MessageController {
-=======
-export class ChatController {
->>>>>>> origin/master:src/chat/chat.controller.ts
     constructor(
         private readonly messageService: MessageService
     ){}
 
-<<<<<<< HEAD:src/rockgram/message/message.controller.ts
-    @Get('/:chatId/:senderId')
-    findAllMessages(@Param(':id') chatId: number, @Param(':id') senderId: number){
-       return this.messageService.getMessages(chatId,senderId )
-=======
     @Get('/:chatId')
     findAllMessages(@Param('chatId') chatId: number){
-       return this.chatService.getMessages(chatId);
->>>>>>> origin/master:src/chat/chat.controller.ts
+       return this.messageService.getMessages(chatId)
+
     }
 
     @Post()
     createMessage(@Body() message: CreateMessageDto){
-<<<<<<< HEAD:src/rockgram/message/message.controller.ts
         return this.messageService.createMessage(message);
     }
 
     @Patch()
-    updateMessage(@Param(':id') id: number, @Body() updatedMessage: UpdateMessageDto){
+    updateMessage(@Param('id') id: number, @Body() updatedMessage: UpdateMessageDto){
         return this.messageService.updateMessage(id, updatedMessage);
     } 
 
     @Delete(':id')
-    deleteMessage(@Param(':id') id: number){
-        return this.messageService.deleteMessage(id);
-    }
-=======
-        const userID = this.getUserId();
-        if(message.senderId != userID){
-            throw new BadRequestException("sender id not matched");
-        }
-        return this.chatService.createMessage(message);
-    }
-
-    @Patch(':id')
-    updateMessage(@Param('id') id: number, @Body() updatedMessage: UpdateMessageDto){
-        return this.chatService.updateMessage(id, updatedMessage);
-    } 
-
-    @Delete(':id')
     deleteMessage(@Param('id') id: number){
-        const userID = this.getUserId();
-        return this.chatService.deleteMessage(id,userID);
+        const userId = this.getUserId();
+        return this.messageService.deleteMessage(id,userId);
     }
 
     getUserId(){
-        // Asume this is reading from JWT
         return 1;
     }
->>>>>>> origin/master:src/chat/chat.controller.ts
 }
--- src/rockgram/message/message.service.ts
+++ src/rockgram/message/message.service.ts
@@ -3,15 +3,10 @@ import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
 
 @Injectable()
-<<<<<<< HEAD:src/rockgram/message/message.service.ts
 export class MessageService {
-  getMessages(chatId: number, senderId: number) {
-    return `load all messages with this chat id: ${chatId}, messages of sender ${senderId} apear in right side of chat`;
-=======
-export class ChatService {
+  
   getMessages(chatId: number) {
-    return `load all messages with this chat id: ${chatId}, messages of sender  apear in right side of chat`;
->>>>>>> origin/master:src/chat/chat.service.ts
+    return `load all messages with this chat id: ${chatId}.`;
   }
   createMessage(message: CreateMessageDto) {
     return 'return the message text';
@@ -19,12 +14,8 @@ export class ChatService {
   updateMessage(id: number, message: UpdateMessageDto) {
     return 'return the updated message text';
   }
-<<<<<<< HEAD:src/rockgram/message/message.service.ts
-  deleteMessage(id: number) {
-=======
 
   deleteMessage(id: number,userId:number) {
->>>>>>> origin/master:src/chat/chat.service.ts
     return 'message deleted';
   }
 }
